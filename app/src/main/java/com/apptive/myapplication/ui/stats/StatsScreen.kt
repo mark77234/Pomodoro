59c5efbd74b06373e9ae8da272ea03124cca56ec
구민서 <minseo12379@gmail.com>
2025-11-15 17:54:01 +0900

    feat - (통계 탭) '학습 통계' 제목 추가 (#5)
    * feat: pie chart 아이콘 추가
    
    * feat: 제목 추가

--- app/src/main/java/com/apptive/myapplication/ui/stats/StatsScreen.kt
+++ app/src/main/java/com/apptive/myapplication/ui/stats/StatsScreen.kt
@@ -1,22 +1,35 @@
 package com.apptive.myapplication.ui.stats
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun StatsTab(modifier: Modifier = Modifier) {
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
-        contentAlignment = Alignment.Center
     ) {
-        Text(text = "홈탭에서 습관 달성률 작업을 진행해주세요.")
+        Text(
+            text = "학습 통계",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = "오늘의 습관 달성 현황",
+            fontSize = 15.sp,
+            color = Color.Gray
+        )
     }
 }
