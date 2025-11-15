c33d406d79dcfea5102719c732586dcb8bdfef61
MINSEO KIM <flower6713@naver.com>
2025-11-15 17:59:40 +0900

    feat - (타이머 탭) '뽀모도로 타이머' 제목 추가  (#6)
    * feat : clock 아이콘 추가
    
    * feat: 타이머 제목 추가
    
    * feat: 타이머 제목 추가 (박스 삭제)

--- app/src/main/java/com/apptive/myapplication/ui/timer/TimerScreen.kt
+++ app/src/main/java/com/apptive/myapplication/ui/timer/TimerScreen.kt
@@ -1,22 +1,48 @@
 package com.apptive.myapplication.ui.timer
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TimerTab(modifier: Modifier = Modifier) {
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+            .padding(16.dp)
+
     ) {
-        Text(text = "뽀모도로 타이머를 작업해주세요.")
+        Text(
+            text = "뽀모도로 타이머",
+            modifier = Modifier
+                .padding(bottom = 8.dp),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+
+        )
+        Text(
+            text = "타이머를 시작하세요",
+            color = Color.Gray
+        )
+
+
+
     }
+
+
+
+
+
+
+
+
 }
