b9d46936e252943386ce921f7dca17cf797dbfaa
구민서 <minseo12379@gmail.com>
2025-11-23 15:03:57 +0900

    feat: 습관 달성 현황 배경색 추가 및 카드 생성

--- app/src/main/java/com/apptive/myapplication/ui/stats/StatsScreen.kt
+++ app/src/main/java/com/apptive/myapplication/ui/stats/StatsScreen.kt
@@ -1,17 +1,27 @@
 package com.apptive.myapplication.ui.stats
 
+import android.R.color.white
+import android.view.RoundedCorner
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.apptive.myapplication.ui.theme.backgroundColor
+
 
 @Composable
 fun StatsTab(modifier: Modifier = Modifier) {
@@ -31,5 +41,24 @@ fun StatsTab(modifier: Modifier = Modifier) {
             fontSize = 15.sp,
             color = Color.Gray
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+                .background(
+                    color = backgroundColor,
+                    shape = RoundedCornerShape(16.dp)),
+        ){
+            Column(
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Text(
+                    text = "습관 달성 현황",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
 }
