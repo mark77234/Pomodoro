34c9ac2acc5c4d724294912b1be35829ba7d31d4
구민서 <minseo12379@gmail.com>
2025-12-31 22:13:45 +0900

    refactor: HabitStatus import 경로 수정

--- app/src/main/java/com/apptive/myapplication/ui/stats/components/HabitItem.kt
+++ app/src/main/java/com/apptive/myapplication/ui/stats/components/HabitItem.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.apptive.myapplication.ui.stats.HabitStatus
+import com.apptive.myapplication.model.HabitStatus
 
 @Composable
 fun HabitItem(habitName: String) {
