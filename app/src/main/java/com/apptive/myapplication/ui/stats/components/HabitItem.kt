395ce2a603def6f0b8de3d2db94675ec81105685
구민서 <minseo12379@gmail.com>
2026-01-01 23:54:54 +0900

    feat: 습관 이름과 상태를 받아 표시하도록 수정

--- app/src/main/java/com/apptive/myapplication/ui/stats/components/HabitItem.kt
+++ app/src/main/java/com/apptive/myapplication/ui/stats/components/HabitItem.kt
@@ -2,19 +2,10 @@ package com.apptive.myapplication.ui.stats.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,11 +14,10 @@ import androidx.compose.ui.unit.sp
 import com.apptive.myapplication.model.HabitStatus
 
 @Composable
-fun HabitItem(habitName: String) {
-    var currentStatus by remember {
-        mutableStateOf(HabitStatus.NOT_COMPLETED)
-    }
-
+fun HabitItem(
+    habitName: String,
+    status: HabitStatus
+) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -39,8 +29,7 @@ fun HabitItem(habitName: String) {
             .padding(15.dp)
     ) {
         Row(
-            modifier = Modifier
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -49,25 +38,15 @@ fun HabitItem(habitName: String) {
                 fontSize = 17.sp,
             )
             Text(
-                text = currentStatus.displayText,
+                text = status.displayText,
                 fontSize = 13.sp,
-                color = currentStatus.textColor,
+                color = status.textColor,
                 modifier = Modifier
                     .background(
-                        color = currentStatus.bgColor,
+                        color = status.bgColor,
                         shape = RoundedCornerShape(50.dp)
                     )
-                    .clickable{
-                        currentStatus = when (currentStatus) {
-                            HabitStatus.NOT_COMPLETED -> HabitStatus.IN_PROGRESS
-                            HabitStatus.IN_PROGRESS -> HabitStatus.COMPLETED
-                            HabitStatus.COMPLETED -> HabitStatus.NOT_COMPLETED
-                        }
-                    }
-                    .padding(
-                        horizontal = 10.dp,
-                        vertical = 4.dp
-                    )
+                    .padding(horizontal = 10.dp, vertical = 4.dp)
             )
         }
     }
