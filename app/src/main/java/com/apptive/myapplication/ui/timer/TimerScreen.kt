57c33df1a83dcdf3394cd7c756fcf51ce02ad971
mark <mark77234@naver.com>
2026-01-04 17:33:56 +0900

    fix: 타이머 배경 색상 수정

--- app/src/main/java/com/apptive/myapplication/ui/timer/TimerScreen.kt
+++ app/src/main/java/com/apptive/myapplication/ui/timer/TimerScreen.kt
@@ -1,13 +1,22 @@
 package com.apptive.myapplication.ui.timer
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -57,7 +66,7 @@ fun TimerTab() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(0xFFE5E7EB))
+            .background(color = Color.White)
             .verticalScroll(scrollState) // 스크롤 가능하게 설정
             .padding(16.dp)
     ) {
@@ -98,8 +107,11 @@ fun TimerTab() {
                         timeLeft--
                     }
                     isStarted = !isStarted
-                                    },
-                onReset = { isStarted = false; isWorking = true; timeLeft = if (is50minMode) 50 * 60 else 30 * 60 }
+                },
+                onReset = {
+                    isStarted = false; isWorking = true; timeLeft =
+                    if (is50minMode) 50 * 60 else 30 * 60
+                }
             )
 
         }
